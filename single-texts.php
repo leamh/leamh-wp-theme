e5ec26ef72a5e4eac05452a515b37821a3831b8c
Jeremy Boggs <jeremyboggs@gmail.com>
2016-10-06 17:12:16 -0400

    Display Source under text title.

--- single-texts.php
+++ single-texts.php
@@ -8,6 +8,7 @@
                     <div class="row">
                         <div class="col-sm-12">
             <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            <?php if ($source = get_post_meta( $post->ID, 'Source', true )): ?><p><?php echo $source; ?></p><?php endif; ?>
                         </div>
                     </div>
                   <ul class="nav nav-tabs" role="tablist">
