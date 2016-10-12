37d35be70e35d3859a9a77552a643a62070a2f63
Jeremy Boggs <jeremyboggs@gmail.com>
2016-10-12 06:58:22 -0400

    Updates footer. Fixes #18.

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@
 				<div class="row">
 					<div class="col-sm-6">
 						<ul class="nav nav-pills">
-							<li role="presentation"><a href="#" class="dead">&COPY; 2016 REMI Project</a></li>
               <li role="presentation"><a href="<?php echo get_permalink('6'); ?>">About the Project</a></li>
+              <li><a href="mailto:feedback@leamh.org">Questions and Feedback?<a></li>
 						</ul>
 					</div>
 					<div class="col-sm-6">
