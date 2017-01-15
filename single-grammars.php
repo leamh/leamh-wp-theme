cce0e95dee136873d74ca0e7647eb12fc454d2f4
Jeremy Boggs <jeremyboggs@gmail.com>
2017-01-15 11:37:52 -0500

    Fixes display of sources on grammar entry.

--- single-grammars.php
+++ single-grammars.php
@@ -36,30 +36,28 @@ if ($terms = wp_get_post_terms($post->ID, 'grammars-category')) {
       <div class="col-sm-6">
 <h4>Sources</h4>
 <p><a href="#" class="toggleAll">All</a></p>
-<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-<?php $books = get_books_for_post(get_the_ID()); ?>
-<?php if (!empty($books)) : foreach ($books as $book_key): ?>
-
-<?php $book = get_post($book_key); ?>
+ <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+<?php $books = get_books_for_post(get_the_id()); ?>
+<?php if (!empty($books)) : foreach ($books as $book): $book_key = $book->ID; ?>
 
 <div class="panel panel-default">
   <div class="panel-heading" role="tab" id="heading-<?php echo $book_key; ?>">
     <h4 class="panel-title"> <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-<?php echo $book_key; ?>" aria-expanded="false" aria-controls="collapse-<?php echo $book_key; ?>"><?php echo $book->post_title; ?></a></h4>
     </div>
     <div id="collapse-<?php echo $book_key; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading">
-<div class="panel-body">
+      <div class="panel-body">
       <div class="definition">
           <?php echo get_post_meta(get_the_ID(), 'book-'.$book_key, true); ?>
       </div>
       <p class="citation"><a href="<?php echo get_permalink($book); ?>"><?php echo $book->post_title; ?>.</a>
       <?php if ($authors = leamh_display_people($book->ID)): ?>
-      <span class="authored-by"><?php echo $authors; ?>.</span>
+      <span class="authored-by">Author: <?php echo $authors; ?>.</span>
       <?php endif; ?>
       <?php if ($editors = leamh_display_people($book->ID, 'editors')) :?>
-      <span class="edited-by">Edited by <?php echo $editors; ?>.</span>
+      <span class="edited-by">Editor: <?php echo $editors; ?>.</span>
       <?php endif; ?>
       <?php if ($translators = leamh_display_people($book->ID, 'translators')) :?>
-      <span class="translated-by">Translated by <?php echo $translators; ?>.</span>
+      <span class="translated-by">Translator: <?php echo $translators; ?>.</span>
       <?php endif; ?>
       </div>
     </div>
@@ -67,6 +65,7 @@ if ($terms = wp_get_post_terms($post->ID, 'grammars-category')) {
 
 <?php endforeach; endif; ?>
 </div>
+
       </div>
     </div>
 </div>
